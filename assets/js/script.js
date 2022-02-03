e3621551439ee9bf1ac9520002326542dfe2f168
compuwill <wpvorngsam@hotmail.com>
2022-02-02 20:35:39 -0600

    some more js updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 
+//Music Preload
+var audMusic = new Audio('./assets/audio/Pokemon Red, Yellow, Blue Battle Music- Trainer_360P - Copy.mp4')
+audMusic.volume = 0.5
+
+//global point values
 var water = 0;
 var fire = 0;
 var fighting = 0;
@@ -27,29 +32,49 @@ var questions = [
 		]
 	},
 	{
-		"question":"In front of you is a thug threatening to take your money. What do you do?",
+		"question":"Which country would you rather travel to?",
 		"answers":[{
-				"answer":"Splash",
-				"points":[{"water":1}]
+				"answer":"The Bahamas",
+				"points":[{"water":1,"normal":1,"fire":1}]
 			},
 			{
-				"answer":"Punch",
-				"points":[{"fighting":1,"rock":1}]
+				"answer":"Japan",
+				"points":[{"fighting":1,"rock":1,"ground":1}]
 			},
 			{
-				"answer":"Run Away",
-				"points":[{"fighting":1,"rock":1}]
+				"answer":"New Zealand",
+				"points":[{"grass":1,"ground":1}]
 			},
 			{
-				"answer":"Solar Beam",
-				"points":[{"grass":1}]
+				"answer":"Mexico",
+				"points":[{"fire":2}]
+			}
+		]
+	},
+    {
+		"question":"Which country would you rather travel to?",
+		"answers":[{
+				"answer":"The Bahamas",
+				"points":[{"water":1,"normal":1,"fire":1}]
+			},
+			{
+				"answer":"Japan",
+				"points":[{"fighting":1,"rock":1,"ground":1}]
+			},
+			{
+				"answer":"New Zealand",
+				"points":[{"grass":1,"ground":1}]
+			},
+			{
+				"answer":"Mexico",
+				"points":[{"fire":2}]
 			}
 		]
 	}
 ]
 
 
-var addPoints = function(answerIndex,answersArray) {    
+var answerQuestion = function(answerIndex,answersArray) {    
     answersArray[answerIndex].points.forEach(element => {
         console.log(element);
     });
@@ -58,9 +83,8 @@ var addPoints = function(answerIndex,answersArray) {
 
 
 
-
+// A bunch of tests on pulling data from the array
 console.log("QUESTION 1:" + questions[0].question)
 
-addPoints(1,questions[0].answers)
-
-addPoints(1,questions[1].answers)
+answerQuestion(1,questions[0].answers)
+answerQuestion(1,questions[1].answers)
